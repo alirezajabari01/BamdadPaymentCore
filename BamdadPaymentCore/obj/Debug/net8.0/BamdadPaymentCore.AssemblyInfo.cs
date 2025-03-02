66b8560be95cfa1d295f96f5756f7ba40760c92b
alirezajabari01 <alireza.jabarii2001@gmail.com>
2025-03-02 10:48:09 +0330

    bef

--- BamdadPaymentCore/obj/Debug/net8.0/BamdadPaymentCore.AssemblyInfo.cs
+++ BamdadPaymentCore/obj/Debug/net8.0/BamdadPaymentCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BamdadPaymentCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4b4be2387eedc7e6c23a9cf20eeafe632d246ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4619b8aa4e25d5456373c208497adde0558fe392")]
 [assembly: System.Reflection.AssemblyProductAttribute("BamdadPaymentCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BamdadPaymentCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
